40cc3e199995255435b81f22bd4c37582d5f76a9
Nico Schuck <nico.schuck@gmail.com>
2025-03-24 22:09:02 +0100

    Initial commite of Nico's network file

---
+++ nico_network.py
@@ -0,0 +1,14 @@
+# nico's main runner file for the network
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torchvision import transforms
+import os
+
+
+
