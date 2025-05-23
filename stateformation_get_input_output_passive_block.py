b0bc98bf068da25b68657f8a206562ea4a19df61
Nico Schuck <nico.schuck@gmail.com>
2025-05-23 16:17:31 +0200

    updated input / output scripts

---
+++ stateformation_get_input_output_passive_block.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 25 07:38:01 2025
+
+@author: grossman
+"""
+import numpy as np
+import pandas as pd
+#%%
+def get_input_output_stateformation(sub,csvpath,isRecurrent,withPassive):
+    # Some hardcode params of the task:
+    n_feat_itemA=2 # 2 binary client features (male/female, glasses/no glasses)
+    n_feat_itemB=3 # 3 binary house features (forest/mountain, swing/pool, )
+    n_actions=2 # 2 choice alternative  - left house / right house
+    n_input_nodes = 16 # 16 inputs node - each 2 nodes code a single binary featue, 8 features altogether (client + 2 houses)
+    reward_weights = np.array([-1,2,0,0,-1,1,-3,3,0,0]) #weights of each feature - client f1, client f2, house f1, house f2, house f3   
+    if sub=='randomized_episode':
+        inputs = get_active_trials_input_comb(n_input_nodes,n_actions,n_feat_itemA) #inputs: 192X16 array (need to break into 2 time points when passed forward)
+        maxtrials = inputs.shape[0]
+        assert(maxtrials==192)
+        
+    else:
+        # Load CSV summerizing behaviour of all participants
+        log_df = pd.read_csv(csvpath + 'State_Formation_behaviour.csv')
+        
+        
+        if withPassive:
+            # Filter active and assive trials in day 1 of selected subject 
+            csub_log = log_df[(log_df['sub']==sub) & (log_df['block_type_num']<12)]
+            
+        else:
+            # Filter active trials in day 1 of selected subject
+            csub_log = log_df[(log_df['sub']==sub) & (log_df['is_passive']==0) & (log_df['block_type_num']<12)]
+            
+        # Convert inputs from strings to numpy array
+        tmp = np.array(csub_log.input)
+        inputs = np.array([np.fromstring(tmp[i][1:-1], dtype=int, sep=' ') for i in range(len(tmp))])
+    
+    # Break input into 2 time point (2X16 instead of 1X16), and compute reward (i.e. target output):
+    n_trials = inputs.shape[0]
+    outputs = np.zeros((n_trials,2)) #trials X outputnodes
+    states = np.empty(n_trials, dtype=object)
+    passive_type = np.empty(n_trials, dtype=object)
+    block = np.zeros((n_trials))
+    block[:] = np.nan
+    recurrent_inputs = np.zeros((n_trials,2,16)) # trials X time points X input nodes 
+    outputs[:],recurrent_inputs[:], states[:] = np.nan, np.nan, np.nan
+    isPassive = np.zeros(n_trials)
+    for itrial in range(n_trials):
+        recurrent_inputs[itrial,0,:]=np.hstack((inputs[itrial][:4],np.zeros(12)))
+        recurrent_inputs[itrial,1,:]=np.hstack((np.zeros(4),inputs[itrial][4:]))
+        cinputs = recurrent_inputs[itrial,:,:]
+        block[itrial] = csub_log.iloc[itrial].block_type_num
+        
+        itrial_inblock = sum(block==block[itrial])-1
+        
+        
+        # Compute reward (i.e, Netwokks tragt output for left and right output nodes)
+        if np.all(cinputs[1,10:]==0): # passive trial - single house is presented (coded in the left house position at cinputs[1,4:10])
+            outputs[itrial,0] = rewardfun(cinputs, 0, reward_weights, n_feat_itemA, n_feat_itemB, n_actions, n_input_nodes) 
+            outputs[itrial,1],isPassive[itrial]  = np.nan, 1 #insert nan in output for right house
+            
+            if sub=='sub501' or 'sub502':
+                if itrial_inblock<5:
+                    passive_type[itrial] = 'pre_early'
+                    
+                elif itrial_inblock>7 and itrial_inblock<12:
+                    passive_type[itrial] = 'post_early'
+                    
+                elif itrial_inblock>27 and itrial_inblock<32:
+                    passive_type[itrial_inblock] = 'pre_late'
+                    
+                elif itrial_inblock>35 and itrial_inblock<40:
+                    passive_type[itrial] = 'post_late'
+                
+            else:
+                if itrial_inblock<5:
+                    passive_type[itrial] = 'pre_early'
+                    
+                elif itrial_inblock>7 and itrial_inblock<12:
+                    passive_type[itrial] = 'post_early'
+                    
+                elif itrial_inblock>27 and itrial_inblock<33:
+                    passive_type[itrial_inblock] = 'pre_late'
+                    
+                elif itrial_inblock>36 and itrial_inblock<42:
+                    passive_type[itrial] = 'post_late'
+
+        else: # active trial - 2 possible outputs
+            outputs[itrial,:] = [rewardfun(cinputs, x, reward_weights, n_feat_itemA, n_feat_itemB, n_actions, n_input_nodes) for x in range(n_actions)]
+            
+        # Log state code
+        states[itrial] = input2state(inputs[itrial][np.array([0,1,4,5,6,7,10,11,12,13])])
+
+    if isRecurrent:
+        inputs = recurrent_inputs.copy()
+ 
+    return inputs, outputs, isPassive, states, block, passive_type
+
+#%% Define a function that generates a shuffled episode of 192 trials (all possible inputs)
+def get_active_trials_input_comb(n_input_nodes,n_actions,n_feat_itemA):       
+
+    # make inputs by combining all choice options
+    itemA_options = np.array([[0, 1, 0, 1],
+                           [0, 1, 1, 0],
+                           [1, 0, 0, 1],
+                           [1, 0, 1, 0]])
+    itemB_options = np.array([[0,1,0,1,0,1],[0,1,0,1,1,0],
+                             [0,1,1,0,0,1],[0,1,1,0,1,0],
+                             [1,0,0,1,0,1],[1,0,0,1,1,0],
+                             [1,0,1,0,0,1],[1,0,1,0,1,0]])
+                             #all possible options for a single itemB
+    inputs = np.zeros((itemA_options.shape[0]*itemB_options.shape[0]*(itemB_options.shape[0]-1), n_input_nodes)) # set array with all possible inputs (each row is an input for a single decision stage (i.e. itemA/itemB); entries 0-3 represent compA, entries 4-8 represent compB )
+    inputs_itemA = np.zeros(itemA_options.shape[0]*itemB_options.shape[0]*(itemB_options.shape[0]-1))
+    inputs_itemBacomp = np.zeros(itemA_options.shape[0]*itemB_options.shape[0]*(itemB_options.shape[0]-1))
+    inputs_itemBbcomp = np.zeros(itemA_options.shape[0]*itemB_options.shape[0]*(itemB_options.shape[0]-1))
+    trial_idx = -1
+    
+    for itemA in np.arange(0,itemA_options.shape[0]):
+        for itemBacomp in np.arange(0, itemB_options.shape[0]):  
+            for idx, itemBbcomp in enumerate(np.setdiff1d(np.arange(0, itemB_options.shape[0]), itemBacomp)):
+                if ~(itemB_options[itemBacomp,0:4]==itemB_options[itemBbcomp,0:4]).all():
+                    trial_idx = trial_idx+1
+                    inputs[trial_idx, :] = np.concatenate([itemA_options[itemA,:], itemB_options[itemBacomp,:], itemB_options[itemBbcomp]])
+                    inputs_itemA[trial_idx]=itemA
+                    inputs_itemBacomp[trial_idx]=itemBacomp
+                    inputs_itemBbcomp[trial_idx]=itemBbcomp
+    
+    inputs=inputs[:trial_idx+1,:]
+    inputs_itemA=inputs_itemA[:trial_idx+1]
+    inputs_itemBacomp=inputs_itemBacomp[:trial_idx+1]
+    inputs_itemBbcomp=inputs_itemBbcomp[:trial_idx+1]
+ 
+    return inputs 
+
+#%% Define a function that gets an input and returns reward for selected action (0--> left house, 1--> right house)
+def rewardfun(cinputs, selected_action, reward_weights, n_feat_itemA, n_feat_itemB,n_actions, n_input_nodes): 
+    # geberate choicemap: mask inputs based on each of 2 possible choices
+    choicemap = np.empty((n_actions, n_input_nodes))
+    choicemap[0, :] = np.concatenate((np.repeat(1, n_feat_itemA*2), np.repeat(1, n_feat_itemB*2), np.repeat(0, n_feat_itemB*2)))# itemB:1
+    choicemap[1, :] = np.concatenate((np.repeat(1, n_feat_itemA*2), np.repeat(0, n_feat_itemB*2), np.repeat(1, n_feat_itemB*2)))# itemB:2
+    
+    cidx = np.flatnonzero(choicemap[selected_action, :])
+    reward_weights_tmp = reward_weights.copy()
+    R = np.dot(cinputs[0,cidx[np.arange(0, n_feat_itemA*2)]], reward_weights_tmp[0:n_feat_itemA*2]) \
+        * np.dot(cinputs[1,cidx[np.arange(n_feat_itemA*2,n_feat_itemA*2+n_feat_itemB*2)]], reward_weights_tmp[n_feat_itemA*2:n_feat_itemA*2+n_feat_itemB*2] ) 
+    R=(R*6.25+50)/100 
+    return(R)
+
+
+#%% Define a function that recieves input array/state codes and returns state code/input array, respectively   
+def input2state(x):
+    # a1/b1 = 0,1,0,1
+    # a2/b2 = 0,1,1,0
+    # a3/b3 = 1,0,0,1
+    # a4/b4 = 1,0,1,0
+     
+    state_2_input_dict = {'a1a2': np.array([1,0,0,1,0,1,0,1,1,0]),
+                              'a2a1': np.array([1,0,0,1,1,0,0,1,0,1]),
+                              'a1a3': np.array([1,0,0,1,0,1,1,0,0,1]),
+                              'a3a1': np.array([1,0,1,0,0,1,0,1,0,1]),
+                              'a1a4': np.array([1,0,0,1,0,1,1,0,1,0]),
+                              'a4a1': np.array([1,0,1,0,1,0,0,1,0,1]),
+                              'a2a3': np.array([1,0,0,1,1,0,1,0,0,1]),
+                              'a3a2': np.array([1,0,1,0,0,1,0,1,1,0]),
+                              'a2a4': np.array([1,0,0,1,1,0,1,0,1,0]),
+                              'a4a2': np.array([1,0,1,0,1,0,0,1,1,0]),
+                              'a3a4': np.array([1,0,1,0,0,1,1,0,1,0]),
+                              'a4a3': np.array([1,0,1,0,1,0,1,0,0,1]),
+                              'b1b2': np.array([0,1,0,1,0,1,0,1,1,0]),
+                              'b2b1': np.array([0,1,0,1,1,0,0,1,0,1]),
+                              'b1b3': np.array([0,1,0,1,0,1,1,0,0,1]),
+                              'b3b1': np.array([0,1,1,0,0,1,0,1,0,1]),
+                              'b1b4': np.array([0,1,0,1,0,1,1,0,1,0]),
+                              'b4b1': np.array([0,1,1,0,1,0,0,1,0,1]),
+                              'b2b3': np.array([0,1,0,1,1,0,1,0,0,1]),
+                              'b3b2': np.array([0,1,1,0,0,1,0,1,1,0]),
+                              'b2b4': np.array([0,1,0,1,1,0,1,0,1,0]),
+                              'b4b2': np.array([0,1,1,0,1,0,0,1,1,0]),
+                              'b3b4': np.array([0,1,1,0,0,1,1,0,1,0]),
+                              'b4b3': np.array([0,1,1,0,1,0,1,0,0,1]),
+                              'a1_l': np.array([1,0,0,1,0,1,0,0,0,0]),
+                              'a2_l': np.array([1,0,0,1,1,0,0,0,0,0]),
+                              'a3_l': np.array([1,0,1,0,0,1,0,0,0,0]),
+                              'a4_l': np.array([1,0,1,0,1,0,0,0,0,0]),
+                              'b1_l': np.array([0,1,0,1,0,1,0,0,0,0]),
+                              'b2_l': np.array([0,1,0,1,1,0,0,0,0,0]),
+                              'b3_l': np.array([0,1,1,0,0,1,0,0,0,0]),
+                              'b4_l': np.array([0,1,1,0,1,0,0,0,0,0]),
+                              'a1_r': np.array([1,0,0,0,0,0,0,1,0,1]),
+                              'a2_r': np.array([1,0,0,0,0,0,0,1,1,0]),
+                              'a3_r': np.array([1,0,0,0,0,0,1,0,0,1]),
+                              'a4_r': np.array([1,0,0,0,0,0,1,0,1,0]),
+                              'b1_r': np.array([0,1,0,0,0,0,0,1,0,1]),
+                              'b2_r': np.array([0,1,0,0,0,0,0,1,1,0]),
+                              'b3_r': np.array([0,1,0,0,0,0,1,0,0,1]),
+                              'b4_r': np.array([0,1,0,0,0,0,1,0,1,0])} #for the sake of simplicity we take only those for the left hand side
+    
+    
+    # Reverse dictionary function
+    def reverse_dict(d):
+        return {tuple(v): k for k, v in d.items()}
+    
+    # Create reversed dictionaries
+    input_2_state_dict = reverse_dict(state_2_input_dict)
+    #example ussage: input_2_state_dict[tuple(input_array)]
+    
+    if isinstance(x, str): #convert state to array
+        y = state_2_input_dict[x]
+    elif isinstance(x, np.ndarray): #convert array to state
+        y = input_2_state_dict[tuple(x)]
+    
+    return(y)
---
+++ stateformation_get_input_output_passive_block_call_example.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 25 10:58:31 2025
+
+@author: grossman
+"""
+# Import helper functions from stateformation_get_input_output.py:
+from stateformation_get_input_output import get_input_output_stateformation
+from stateformation_get_input_output import get_active_trials_input_comb
+from stateformation_get_input_output import rewardfun
+from stateformation_get_input_output import input2state
+
+subjects =  ['sub501', 'sub502', 'sub503', 'sub504', 'sub505', 'sub506', 'sub507', 'sub508', 'sub509', 'sub510',
+  'sub511', 'sub512', 'sub513', 'sub514', 'sub515', 'sub516', 'sub517', 'sub518', 'sub519', 'sub520', 
+  'sub521', 'sub522', 'sub523', 'sub524', 'sub525', 'sub526', 'sub527', 'sub528', 'sub529', 'sub530', 
+  'sub531', 'sub532', 'sub533', 'sub534', 'sub535', 'sub536', 'sub537', 'sub538','sub540', 
+  'sub541', 'sub543',  'sub545', 'sub546', 'sub547', 'sub548', 'sub549', 'sub550', 
+  'sub551', 'sub552', 'sub555', 'sub556', 'sub557', 'sub558', 'sub559', 'sub560', 
+  'sub561', 'sub562', 'sub563', 'sub564'] 
+
+#%% Define manually:
+csvpath = '/Users/grossman/Volumes/STATE_FORMATION/MRI_DATA/MRI_REAL/LOGs/' #  
+# Call the function the brings back input and outputs:
+inputs, outputs, isPassive, states, block, passive_type = get_input_output_stateformation(sub='sub511',csvpath = csvpath, isRecurrent=True, withPassive = True)   
+
+ # INPUTS:
+     # sub: either a real participant from the state formation experiment, e.g. 'sub508' see list of available particiapnts below 
+     #      or, if sub='randomized_episode', returns a suffelled input of the 192 unique inputs of the task
+     # csvpath: see above, path to where to place the State_Formation_behaviour.csv
+     # isRecurrent: - if True, inputs are returnet as a 2X16 array per input, first row holds the nodes of the context and 2nd row the nodes of the houses
+     #              - if False, inputs are retured as 1X16 array per input - both client and house pair presented simultanously to the network
+     # withPassive: If True, the outputs include also passive trials inserted pre and post active trials segments.
+     #              Passive trials inputs include a single house (not a pair), encoded in the left house position (inputs[i,1,4:10]), 
+     #              the right house input is all zeros (inputs[i,1,10:]).
+     #              For passive trials, the output arrays will have only the value of the left house (outputs[i,0]) and nan in the right house (outputs[i,1])
+                     
+ # OUTPUTS:
+     # inputs: [n_trialsX n time points X n input nodes] numpy array, to get input of the ith trial  inputs[i,:,:]
+     # outputs: [n_trials X n outcomes] numpy array: for each tial, the reward for the left [i,0] and right [i,1] house
+     # isPassive: [n_trials] array with 0 for active trials and 1 for passive trials 
+     # states: [n_trials] arrays of strings stating the state on each trial. Active trials are encoded as e.g. a1a2, meaning context a with house type 1 on the left and house type 2 on the right.
+     #         passive trials are encoded as e.g., b2_l, meaning context b with house type 2 on left position (position is not relevant for a serial architecture). All passive inputs are encoded as positioned on the left by default.
+     # block: [n_trial] araay with the learning blokc number. 11 blocks alltogether, each contains 42 trials (aside from sub501, sub502 for whom it has 40 trials each)
+     # passive_type: [n_trial] which indivates fpr passive trials, to which segment they belong in the block (pre_early /  post_early / pre_late /  post_late)
+     #         A single learning block has this structure: 
+     #         4 passive trials (pre_early) --> 4 active trials --> 4 passive trials repeat (post_early) --> 16 active trials --> 5 passive trials (pre_late) --> 4 active trials --> 5 passive trials (post_late)
+     #         pre_late and post_late contain 5 passive trials, since one of the 5 states  was also present in the active trials coming inbetween (we added these as of sub503 to get a some ceiling measure of neural change)
+
+# all subjects names:
+# ['sub501', 'sub502', 'sub503', 'sub504', 'sub505', 'sub506', 'sub507', 'sub508', 'sub509', 'sub510',
+#   'sub511', 'sub512', 'sub513', 'sub514', 'sub515', 'sub516', 'sub517', 'sub518', 'sub519', 'sub520', 
+#   'sub521', 'sub522', 'sub523', 'sub524', 'sub525', 'sub526', 'sub527', 'sub528', 'sub529', 'sub530', 
+#   'sub531', 'sub532', 'sub533', 'sub534', 'sub535', 'sub536', 'sub537', 'sub538','sub540', 
+#   'sub541', 'sub543',  'sub545', 'sub546', 'sub547', 'sub548', 'sub549', 'sub550', 
+#   'sub551', 'sub552', 'sub555', 'sub556', 'sub557', 'sub558', 'sub559', 'sub560', 
+#   'sub561', 'sub562', 'sub563', 'sub564'] 
+
+#Subjects to take care of partial blocks: 
+#'sub539', 'sub542', 'sub544', 'sub553' ,'sub554'
